850db543cdd6fc2dd2dcfeb5c3749ca20e5728fd
Vijay <vjpscjp@gmail.com>
2016-04-22 15:20:24 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,98 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
-	"net/http"
+	f "github.com/MustWin/gomarathon"
+	"github.com/gin-gonic/gin"
+	"strings"
+	"os"
 )
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!</h1>\n")
+/*var (
+	url		   = "https://shipped-tx3-control-01.tx3.shipped-cisco.com/marathon"
+	user       = "synthetic-mon"
+	pwd        = "VpYdy5abudqkk3Ts"
+)*/
+
+
+type APP struct{
+	Appid string
 }
 
+type HOST_PORT struct{
+	Host string
+	Port int
+}
+
+
 func main() {
-    http.HandleFunc("/", defaultHandler)
-    fmt.Println("Example app listening at http://localhost:8888")
-    http.ListenAndServe(":8888", nil)
+	os.Setenv("MARATHON_URL","https://shipped-tx3-control-01.tx3.shipped-cisco.com/marathon")
+	os.Setenv("MARATHON_USER","synthetic-mon")
+	os.Setenv("MARATHON_PASS","VpYdy5abudqkk3Ts")
+	
+	url := os.Getenv("MARATHON_URL")
+	user := os.Getenv("MARATHON_USER")
+	pwd := os.Getenv("MARATHON_PASS")
+	
+	r := gin.Default()
+	auth := f.HttpBasicAuth{user, pwd}
+	client, err := f.NewClient(url, &auth, &tls.Config{InsecureSkipVerify: true})
+	if err != nil {
+		fmt.Println("Invalid Details : "+err.Error())
+		return
+	}
+	r.POST("/app",func(c *gin.Context){
+		var a APP
+		err := c.BindJSON(&a)
+		if err != nil{
+			c.JSON(400,gin.H{"message":"Bad Request"})
+			return
+		}
+		
+		appid := strings.Replace(a.Appid, "/", "", -1)
+		resp, err := client.GetApp(appid)
+		if err != nil {
+			fmt.Println("Invalid Details : "+err.Error())
+			return
+		}
+
+		c.JSON(200,resp)		
+	})
+	r.POST("/host_port",func(c *gin.Context){
+		var a HOST_PORT
+		err := c.BindJSON(&a)
+		if err != nil{
+			c.JSON(400,gin.H{"message":"Bad Request"})
+			return
+		}
+		
+		flag := false
+		resp, err := client.ListTasks()
+		if err != nil {
+			c.JSON(400,gin.H{"message":err})
+			return
+		}
+		for _, t := range resp.Tasks {
+			if t.Host == a.Host {
+				for _, p := range t.Ports {
+					//fmt.Println("DEBUG port ", p)
+					if p == a.Port {
+						resp, err := client.GetApp(t.AppID)
+						if err != nil{
+							c.JSON(400,gin.H{"message":"Bad Request"})
+							return
+						}
+						c.JSON(200,resp)		
+						flag = true
+					}
+				}
+			}
+		}
+		if !flag {
+			c.JSON(400,gin.H{"message":"No Record Found"})
+			return
+		}	
+	})
+	r.Run()
 }
